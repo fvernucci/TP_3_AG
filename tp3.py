eb0e66e7d204c33fb162a166995ae9602d9cb80c
florencia <florvernucci@gmail.com>
2023-11-05 11:57:14 -0300

    enunciado

---
+++ tp3.py
@@ -0,0 +1,27 @@
+'''
+Problema
+1. Hay 5 casas.
+2. El Matematico vive en la casa roja.
+3. El hacker programa en Python.
+4. El Brackets es utilizado en la casa verde.
+5. El analista usa Atom.
+6. La casa verde esta a la derecha de la casa blanca.
+7. La persona que usa Redis programa en Java
+8. Cassandra es utilizado en la casa amarilla
+9. Notepad++ es usado en la casa del medio.
+10. El Desarrollador vive en la primer casa.
+11. La persona que usa HBase vive al lado de la que programa en JavaScript.
+12. La persona que usa Cassandra es vecina de la que programa en C#.
+13. La persona que usa Neo4J usa Sublime Text.
+14. El Ingeniero usa MongoDB.
+15. EL desarrollador vive en la casa azul.
+
+¿Quien usa vim?
+'''
+
+#import:
+import random
+import time
+
+#identificar un individio
+
